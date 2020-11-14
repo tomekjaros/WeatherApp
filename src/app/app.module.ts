4e9e9e462b30353658dbf6c061944cf5ae7d4354
Tomek <tmszjaros@gmail.com>
2020-11-14 15:40:24 +0100

    pogoda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {StartComponent} from './component/start/start.component';
+import {SelectCityComponent} from './component/select-city/select-city.component';
+import {ShowWeatherComponent} from './component/show-weather/show-weather.component';
+import {PageNotFoundComponent} from './component/page-not-found/page-not-found.component';
+import {ToolbarComponent} from './component/toolbar/toolbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {HttpClientModule} from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    SelectCityComponent,
+    ShowWeatherComponent,
+    PageNotFoundComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/component/show-weather/show-weather.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {WeatherService} from '../service/weather.service';
+
+@Component({
+  selector: 'app-show-weather',
+  templateUrl: './show-weather.component.html',
+  styleUrls: ['./show-weather.component.css']
+})
+export class ShowWeatherComponent implements OnInit {
+  public actualWeatherData = null;
+  constructor(
+    private weatherService: WeatherService
+  ) { }
+  ngOnInit(): void {
+    this.actualWeatherData = this.weatherService.actualWeatherData;
+  }
+}
